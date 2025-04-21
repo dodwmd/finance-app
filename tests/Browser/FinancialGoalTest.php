5fcbb267aa80962ad4cd8ac84247bd46cb74fe68
dodwmd <1372930+dodwmd@users.noreply.github.com>
2025-04-21 23:51:18 +1000

    tests

--- tests/Browser/FinancialGoalTest.php
+++ tests/Browser/FinancialGoalTest.php
@@ -4,7 +4,6 @@ namespace Tests\Browser;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
 class FinancialGoalTest extends DuskTestCase
